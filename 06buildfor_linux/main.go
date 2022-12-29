68d1832c35f31c29b29d16ff7c2ad6272aee03c6
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-12-30 04:49:55 +0530

    build for linux lec 11

---
+++ 06buildfor_linux/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+	go give us freedom to build exe for windows
+	linux executable
+	and for mac also
+
+	so lets copy code from previous section and lets go and build a executable
+	file for liux
+
+	it gonna happen by command
+	"go build"
+
+
+*/
+
+func main() {
+	fmt.Println("lets start learning time")
+	presenttime := time.Now() // we are using a built in libaray known as time
+	fmt.Println(presenttime)
+	/*
+		this will give output as{output is when i am using it can change with time}
+		2022-12-30 04:23:34.087325259 +0530 IST m=+0.000051117
+	*/
+
+	/*
+		but i dont want like this
+		we need to format it like we did in javascript
+	*/
+
+	fmt.Println(presenttime.Format("01-02-2006"))
+	/*
+		WE ARE NOW GONNA USE ANOTHER LIBRARY
+		"Format" lets it will format the time
+
+		so it will give output like
+		12-30-2022
+
+	*/
+	fmt.Println(presenttime.Format("01-02-2006 15:04:05 Monday")) // you can add some more parameter to Format like day,time
+	/*
+		12-30-2022 04:32:55 Friday
+		{output gets replaced by current date}
+
+	*/
+
+	/*
+		NOTE:-
+		BY GIVING A FORMAT IN Format which is ""
+		you  have to write exact thing to get the output replaced by that current time
+
+		for example agar humne Monday likha hai toh output mein Monday replace ho jayega uss din ki date se
+		and same thing with the time also ki agar humne current time to change karna hai toh humne
+		15:04:05 hee likhna padega and if we write something else
+		it not gonna work
+		woh as it is ka as it is utha ke print kardega
+
+	*/
+	createddate := time.Date(2020, time.August, 10, 23, 23, 0, 0, time.UTC)
+	/*
+		WE ARE CRETAING A CUSTOME DATE HERE
+		IT WORKS LIKE
+		time.Date(year,month{always using time.<month>},day,hour,minute,second,nano second , location)
+	*/
+	fmt.Println(createddate)
+
+}
